25ad7d1300fd59ce309dc29e946689c20ef8cb43
rlopezdev <rlopez@linagora.com>
2021-11-03 11:32:48 +0100

    update homepage, add vertical nav active status

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -20,4 +20,66 @@ window.onload = () => {
 
     /* SLIDER */
     $('#homepage-slider').bxSlider();
+
+    /*** SECTIONS NAV ***/
+    const getCurrentBlock = () => {
+        let scrollHeight = document.body.scrollHeight
+        let scrollPosY = window.pageYOffset || document.documentElement.scrollTop
+        let nbBlocks = document.getElementsByTagName('section').length
+        let blockHeight = scrollHeight / nbBlocks
+        return Math.ceil((scrollPosY + blockHeight) / blockHeight)
+    }
+    const goToNextBlock = () => {
+        let currentBlock = getCurrentBlock()
+        let nbBlocks = document.getElementsByTagName('section').length
+        let navBullets = document.getElementsByClassName('vertical-nav-bullet')
+        if (currentBlock < nbBlocks) {
+            let targetIndex = currentBlock + 1
+            for (let nav of navBullets) {
+                let index = nav.getAttribute('data-index')
+                if (parseInt(index) === parseInt(targetIndex)) {
+                    let targetBlock = nav.getAttribute('data-target')
+                    location.hash = targetBlock
+                }
+            }
+        }
+    }
+    const gotToPreviousBlock = () => {
+        let currentBlock = getCurrentBlock()
+        let navBullets = document.getElementsByClassName('vertical-nav-bullet')
+        if (currentBlock > 1) {
+            let targetIndex = currentBlock - 1
+            for (let nav of navBullets) {
+                let index = nav.getAttribute('data-index')
+                if (parseInt(index) === parseInt(targetIndex)) {
+                    let targetBlock = nav.getAttribute('data-target')
+                    location.hash = targetBlock
+                }
+            }
+        }
+    }
+    document.onscroll = (e) => {
+        let currentBlock = getCurrentBlock()
+        let navBullets = document.getElementsByClassName('vertical-nav-bullet')
+        for (let nav of navBullets) {
+            const index = nav.getAttribute('data-index');
+            nav.classList.remove('active')
+            if (parseInt(index) === parseInt(currentBlock)) {
+                nav.classList.add('active')
+            }
+        }
+    }
+    const nextBlockBtn = document.getElementById('nextblock')
+    const prevBlockBtn = document.getElementById('prevblock')
+    nextBlockBtn.onclick = () => {
+        goToNextBlock()
+    }
+    prevBlockBtn.onclick = () => {
+        gotToPreviousBlock()
+    }
+
+
+
+
+
 }
